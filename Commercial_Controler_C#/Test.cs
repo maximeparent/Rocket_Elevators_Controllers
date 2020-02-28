cc2acb7a91156c4c0b17745f534a05797a837580
maximeparent <60401437+maximeparent@users.noreply.github.com>
2020-02-28 13:42:03 -0500

    Add files via upload

---
+++ Commercial_Controler_C#/Test.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using System.Linq;
+
+  static void Main(string[] args)
+//   {
+//     Column1 = new Column(1, 4,);  // Create an object of the Class (this will call the constructor)
+//     Console.WriteLine(Column1.id);  // Print the value of model
+//   }
+
+// class Column
+// {
+//   public int id;  // Create a field
+//   public int nbElevator;
+//   public int nbFloor;
+//   public int elevatorsList; 
+//   public int callButtonList;
+//   public int buttonList;
+
+//   // Create a class constructor for the Car class
+//   public Column()
+//   {
+//   // Use this to qualify the members of the class 
+//     this.id = id;
+//     this.nbElevator = nbElevator;
+//     this.nbFloor = nbFloor;
+//     this.elevatorsList = [];
+//     this.callButtonList = [];
+//     this.buttonList = [];
+//   }
+
+// }
+
+    class Batterie
+    {
+        public int id;
+        public int column;
+        public int requestedFloor;
+
+// Create a class constructor for the Car class
+    public Batterie()
+        {
+// Use this to qualify the members of the class 
+    this.id = id;
+    this.column;
+    this.requestedFloor;
+
+        }
+
+    }
